811f2eda818470435b18b0825b999939beaf9ec8
Schueler-IT <Schueler-IT@LN22-C13-PC08>
2023-11-08 13:00:59 +0100

    schule3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,12 +1,71 @@
 ﻿namespace ConsoleApp3 {
     internal class Program {
         static void Main(string[] args) {
-            int i = 0;
-            while(i <=100) { 
-            Console.WriteLine(i);
-                i++;
+            //AufDie100();
+            //xxxxxxxxx();
+            //Schleife2Mal();
+            //Pyramide();
+            GanzePyramide();
+           Console.ReadKey();
+
+
+
+          
+        }
+        public static void AufDie100() {
+            int result = 0;
+            for (int i = 1; i<= 100; i++) 
+            { 
+            result = result+ i;
             }
-            //test
-        }//test2
+           Console.WriteLine(result); 
+        }
+        public static void xxxxxxxxx() 
+        { 
+            for (int i = 0; i<=10; i++)
+            {
+                Console.Write("xxxxxx\n");
+            }
+        }
+        public static void Schleife2Mal() 
+        {
+           for ( int l = 0; l <= 6; l++)
+           { 
+
+            for (int i = 0; i <= 10; i++) 
+            {
+                Console.Write('x');
+            }
+            Console.WriteLine("");
+           }
+        }   
+        public static void Pyramide() 
+        {
+            for (int l = 0; l <= 14; l++) {
+                 //(int k = 1; k <= 10; k++)
+                 {
+                    for (int i = 0; i <= l ; i++) {
+                        Console.Write('x');
+                    }
+                }
+                
+                Console.WriteLine("");
+            }
+        }
+        public static void GanzePyramide() 
+        {
+            for (int l = 0; l <= 12; l++) {
+                for (int w = 12)
+                {
+                    Console.Write(" ");
+                    for (int i = 0; i <= l; i++) {
+                        
+                        Console.Write('x');
+                    }
+                }
+
+                Console.WriteLine("");
+            }
+        }
     }
 }
