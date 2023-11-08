c86820914f039cb29f2f495df6661be95f333482
Regenschirmtrockner <148333442+Regenschirmtrockner@users.noreply.github.com>
2023-11-08 10:35:31 +0100

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp3 {
+    internal class Program {
+        static void Main(string[] args) {
+            int i = 0;
+            while(i <=100) { 
+            Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
